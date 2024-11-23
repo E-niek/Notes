6b55b3d576ca696505b68d551a27fccfc3931f03
Eniek <121483853+E-niek@users.noreply.github.com>
2024-11-23 13:46:54 +0100

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,98 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<header>
+    <ul class="navigation">
+        <li><a href="/" class="nav-item" class:nav-item-hover={$page.url.pathname === "/"}>Create note</a></li>
+        <li><a href="/about" class="nav-item" class:nav-item-hover={$page.url.pathname === "/about"}>About</a></li>
+    </ul>
+    <a href="/login" class:login-hover={$page.url.pathname === "/login"} id="login">Login</a>
+</header>
+<hr>
+<main>
+    <slot/>
+</main>
+<footer>
+    <hr>
+    <div id="footer-logo-wrapper">
+        Source code:
+        <a href="https://github.com/E-niek/Notes" target="_blank"><img src="/media/github.svg" alt="Link to github project" id="github-logo"></a>
+    </div>
+</footer>
+
+
+<style>
+    :global {
+        * {
+            padding: 0;
+            margin: 0;
+            color: white;
+        }
+
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background-color: #003329;
+        }
+
+        a {
+            text-decoration: none;
+        }
+    }
+
+    header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: transparent;
+    }
+
+    .navigation {
+        display: flex;
+        margin: 16px 0px;
+        list-style: none;
+    }
+
+    .nav-item {
+        margin: 0px 10px;
+        font-size: 17px;
+    }
+
+    .nav-item:hover, .nav-item-hover {
+        border-bottom: 1px solid #0082A1;
+    }
+
+    #login {
+        position: absolute;
+        right: 0;
+        margin: 8px;
+        padding: 7px 10px;
+        border: 1px solid #0082A1;
+        border-radius: 10px;
+    }
+
+    #login:hover, #login.login-hover {
+        border-width: 2px;
+        margin-right: 7px;
+    }
+
+    main {
+        display: flex;
+        flex: 1;
+    }
+
+    #footer-logo-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #github-logo {
+        display: block;
+        width: 30px;
+        margin: 10px 10px;
+    }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<main>
+    <h4 id="label">Enter your note:</h4>
+    <div id="input-wrapper">
+        <div>
+            <label for="preview-toggle">Preview: </label>
+            <input type="checkbox" id="preview-toggle">
+        </div>
+        <textarea id="note"></textarea>
+    </div>
+
+    <button id="button-url">Generate URL</button>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        flex: 1;
+    }
+
+    #label {
+        margin-top: 10px;
+    }
+
+    #input-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        height: 100%;
+        margin-top: 10px;
+    }
+
+    #preview-toggle:checked {
+        accent-color: #0082A1;
+    }
+
+    #note {
+        height: 100%;
+        width: 700px;
+        border: 1px solid white;
+        border-radius: 20px;
+        margin-top: 5px;
+        padding: 10px;
+        color: black;
+        resize: none;
+    }
+
+    #button-url {
+        width: 200px;
+        border: 1px solid white;
+        border-radius: 8px;
+        background-color: #0082A1;
+        margin: 20px 0px;
+        padding: 10px;
+    }
+
+    #button-url:active {
+        scale: .95;
+    }
+</style>
